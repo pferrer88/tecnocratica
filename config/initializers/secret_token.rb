a65fc8aec6dac2f2d09cd599c9a4b65becf5d0f6
Pedro Ferrer <pferrer88@gmail.com>
2013-10-20 18:03:37 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tecnocratica::Application.config.secret_token = 'b16d4ba0f579c9cf2ba704a1a50509ec03716acce688a180eb2b9c1891aa165a5f6e6d54e6dc2c8787da16dca15d0d8d15cd8432f267b6bdff562232d08ee2f9'
